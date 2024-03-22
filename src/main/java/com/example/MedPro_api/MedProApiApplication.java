64a545508d7d84344404f48a1575d4ff81e0199e
Brunofell <brunofelimarti@gmail.com>
2024-03-22 12:48:08 -0300

    first commit

---
+++ src/main/java/com/example/MedPro_api/MedProApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.MedPro_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedProApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedProApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MedPro_api/MedProApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MedPro_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedProApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
