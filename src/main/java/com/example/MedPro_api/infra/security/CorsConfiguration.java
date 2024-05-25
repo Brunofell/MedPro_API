b5d17ed6956fe19b73896817cfee59f4359a6dc0
Bruno Feliciano <brunofelimarti@gmail.com>
2024-05-24 21:05:33 -0300

    cors de novo

--- src/main/java/com/example/MedPro_api/infra/security/CorsConfiguration.java
+++ src/main/java/com/example/MedPro_api/infra/security/CorsConfiguration.java
@@ -10,8 +10,8 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowedHeaders("*");
     }
 }
